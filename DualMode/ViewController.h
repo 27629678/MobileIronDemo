a271274c871611f05e19d778343203ccc8e4eb79
yuxiaohua <hzyuxiaohua@corp.netease.com>
2016-08-16 11:58:55 +0800

    Initial Commit

---
+++ DualMode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DualMode
+//
+//  Created by hzyuxiaohua on 16/8/16.
+//  Copyright © 2016年 hzyuxiaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DualMode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DualMode
+//
+//  Created by hzyuxiaohua on 16/8/16.
+//  Copyright © 2016年 hzyuxiaohua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
